c8e88ffe8591d7bbb794f25d90bbfa41601da716
admxADV <161820059+admxADV@users.noreply.github.com>
2026-02-19 18:22:15 +0300

    Исправление: async функция handleAuthAPI

--- server.js
+++ server.js
@@ -243,7 +243,7 @@ function serveStatic(req, res) {
     }
 }
 
-function handleAuthAPI(req, res) {
+async function handleAuthAPI(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
